f9e82309d7364d15b2a22b348b38c1b1a5eb8f7e
Cody De Arkland <cody@hashicorp.com>
2020-06-01 00:00:53 -0700

    adding vault configs for static secrets

--- app-tier/app/main.py
+++ app-tier/app/main.py
@@ -18,19 +18,15 @@ socketio = SocketIO(app)
 CORS(app)
 thread = None
 
-pghost = os.environ.get('POSTGRES_HOST')
-pguser = os.environ.get('POSTGRES_USER')
-pgpass = os.environ.get('POSTGRES_PASSWORD')
-pgport = os.environ.get('POSTGRES_PORT')
-pgdb = os.environ.get('POSTGRES_DATABASE')
-
+with open('/vault/secrets/db', 'r') as file:
+    connstring = file.read().replace('\n','')
 
 @app.route("/api/post", methods=["POST"])
 def insert_post():
     req = request.get_json()
     _title = req['title']
     _text = req['text']
-    conn = psycopg2.connect(f"host={pghost} port={pgport} dbname={pgdb} user={pguser} password={pgpass}")
+    conn = psycopg2.connect(connstring)
     cur = conn.cursor()
     cur.execute("INSERT INTO textData (title, text) VALUES (%s, %s)", (_title, _text))
     conn.commit()
@@ -44,7 +40,7 @@ def insert_post():
 
 @app.route("/api/posts", methods=["GET"])
 def get_posts():
-    conn = psycopg2.connect(f"host={pghost} port={pgport} dbname={pgdb} user={pguser} password={pgpass}")
+    conn = psycopg2.connect(connstring)
     cur = conn.cursor(cursor_factory=RealDictCursor)
     data = cur.execute('SELECT * FROM textData ORDER BY id DESC')
     test = cur.fetchall()
